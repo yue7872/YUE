e52f1feebd0f8026d225719bafcd683417b9cadf
yue7872 <2258462872@qq.com>
2022-04-16 22:46:43 +0800

    chore: modify detail 。

--- src/utils/compiler.ts
+++ src/utils/compiler.ts
@@ -2,19 +2,20 @@ export default function compiler(templateStr: string): any {
   // <div class="custom">my div</div>
   type Props = Record<string, string>
   // ?表示0次或1次 可以匹配第一次出现的
-  const regx = /<.*?>/
-  const firstTag = templateStr.match(regx)![0].replace('<', '').replace('>', '')
+  const firstTag = templateStr.match(/<.*?>/)![0].replace('<', '').replace('>', '')
   const tagArr = firstTag.split(' ')
   const tagName: string = tagArr[0]
-  tagArr.shift()
   const props: Props = {}
+  let nodeChildren
+
+  tagArr.shift()
   tagArr.forEach((item) => {
     const objKey: any = item.split('=')[0]
     const objValue: string = item.split('=')[1]
     props[objKey] = objValue
   })
-  let nodeChildren
-  const children = templateStr.match(/>.*</)![0].substring(1, templateStr.match(/>.*</)![0].length - 1)
+  const childrenStr = templateStr.match(/>.*</)![0]
+  const children = childrenStr.substring(1, childrenStr.length - 1)
   if (children.includes('<') && children.includes('</'))
     nodeChildren = [compiler(children)]
   else
